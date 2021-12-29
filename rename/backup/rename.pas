593600af41b8574dd71d7ef5f6ab240f10b7eccf
Le Bao Nguyen <bao12345yocoo@gmail.com>
2021-12-30 02:00:44 +0700

    Clean directories

--- rename/backup/rename.pas
+++
@@ -1,9 +0,0 @@
-program rename;
-uses sysutils,crt;
-begin
-	if ParamCount >= 2 then begin
-		RenameFile(ParamStr(1), ParamStr(2));
-		write('File ', ParamStr(1), ' renamed to ', ParamStr(2));
-		exit;
-	end;
-end.
