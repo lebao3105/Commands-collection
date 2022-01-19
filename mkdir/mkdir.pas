3e81a547f5a808d822ea20c8ef27fbb927cab046
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-01-19 11:12:53 +0700

    Edit the help flag and mkdir usage

--- mkdir/mkdir.pas
+++ mkdir/mkdir.pas
@@ -1,26 +1,25 @@
 program mkdir;
 uses sysutils, crt, utils;
 label help;
-var n: integer;
 begin
     if ParamCount = 0 then goto help
-    else  for n := 1 to ParamCount do begin 
-        if ParamStr(n) = '--help' then goto help
-        else begin 
-            If Not DirectoryExists(ParamStr(n)) then
-                If Not CreateDir (ParamStr(n)) Then begin
+    else   
+        if ParamStr(1) = '--help' then goto help
+        else begin  
+            If Not DirectoryExists(ParamStr(1)) then
+                If Not CreateDir (ParamStr(1)) Then begin
                     textred('Failed to create directory !');
                     exit; end
                 else begin
-                    Write('Directory ', ParamStr(n), ' created.');
+                    Write('Directory ', ParamStr(1), ' created.');
                     exit; end
-            else textred('Fatal: '); TextColor(White); writeln('Directory ', ParamStr(n), 'exists!');
+            else textred('Fatal: '); TextColor(White); writeln('Directory ', ParamStr(1), 'exists!');
         end; 
-    end;  
     help:
         begin
             writeln('mkdir usage: mkdir <direcory name>');
             writeln('Use --help flag to print this help, however; you dont need to use this flag yet.');
+            writeln('Sorry about that but mkdir can only create 1 folder at one time.');
             writeln('Exiting... Process exited with code 0 (success).');
             Delay(800);
             exit;
