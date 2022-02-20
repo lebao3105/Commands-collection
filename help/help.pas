4d99df7d8be0bf7bb8e7c9c5cfc7c27c3c009767
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-02-20 14:19:36 +0700

    Update more programs syntax

--- help/help.pas
+++ help/help.pas
@@ -25,33 +25,48 @@ begin
   writeln('3. dir             : Show folder content');
   writeln('4. rm              : Remove file/folder');
   for n := 1 to ParamCount do begin
-    if ParamStr(n) = 'mkdir' then 
+    if ParamStr(n) = 'mkdir' then begin
         writeln('mkdir Syntax:');
         writeln('mkdir [folder name] : Create [folder name] directory.');
-        exit;
-    if ParamStr(n) = 'cls' then 
+        exit; end
+    else if ParamStr(n) = 'cls' then begin 
         writeln('Just use cls to clear the screen, no other arguments needed.');
-        exit;
-    if ParamStr(n) = 'pwd' then
+        exit; end
+    else if ParamStr(n) = 'pwd' then begin
         writeln('Use pwd to show the current folder path. For example, if you are in C:\, pwd will give the');
         writeln('output C:\');
-        exit;
-    if ParamStr(n) = 'touch' then
+        exit; end
+    else if ParamStr(n) = 'touch' then begin
         writeln('touch Syntax:');
         writeln('touch [filename] : Create file [filename].');
-        exit;
-    if ParamStr(n) = 'getvar' then
+        exit; end
+    else if ParamStr(n) = 'getvar' then begin
         writeln('getvar Syntax:');
         writeln('getvar [var] : Get the value of variable [var]');
-        exit;
-    if ParamStr(n) = 'move' then
+        exit; end
+    else if ParamStr(n) = 'move' then begin
         writeln('move Syntax:');
         writeln('move [original file name] [new file name] : Change the old file name to the new one.');
-        exit;
-    if ParamStr(n) = 'find_content' then
+        exit; end
+    else if ParamStr(n) = 'find_content' then begin
         writeln('find_content Syntax:');
         writeln('find_content [text1] [text2] --target [file name] : Find text1/2/... in filename');
+        exit; end
+    else if ParamStr(n) = 'cd' then begin
+        writeln('cd Syntax:');
+        writeln('cd [directory] : Change the current directory to the other'); 
+        exit; end
+    else if ParamStr(n) = 'printf' then begin
+        writeln('printf Syntax: ');
+        writeln('printf [text] --target [file name] : Writes [text] to [file name]');
         exit;
+    end
+    else if ParamStr(n) = 'echo' then begin
+        writeln('echo Syntax: ');
+        writeln('echo [text] : Writes [text] to the screen. Currently we dont support something to break the line like \n.');
+        writeln('If you want to print a variables value, use getvar. For print texts to file use printf instead.');
+        exit;
+    end;
   end;
 end.
 
