407a5486a421e57b49d00ddbb7e24068ef3fca09
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-02-20 14:09:37 +0700

    Create new function for missing dir(s), and add a delay timer

--- rtl/warn.pas
+++ rtl/warn.pas
@@ -5,6 +5,7 @@ function missing_num():integer;
 function missing_argv():integer;
 function missing_file():integer;
 function help():integer;
+function missing_dir():integer;
 Implementation
 uses
     color, crt;
@@ -13,23 +14,35 @@ function missing_num():integer;
 begin
     textred('Fatal: ');
     TextColor(White);
-    writeln('You are missing number(s) to do this action. Exiting...');
+    writeln('You are missing number(s) to do this action. Exitting...');
+    delay(800);
 end;
 function missing_argv():integer;
 begin
     textred('Fatal: ');
     TextColor(White);
-    writeln('You are missing (an) argument(s) for the application. Exiting...');
+    writeln('You are missing (an) argument(s) for the application. Exitting...');
+    delay(800);
 end;
 function missing_file():integer;
 begin
     textred('Fatal: ');
     TextColor(White);
-    writeln('The program needs (one) more file(s) to start. Exiting...');
+    writeln('The program needs (one) more file(s) to start. Exitting...');
+    delay(800);
 end;
 function help():integer;
 begin
     writeln('You may want to use the help program or read the README.md file from the source code.');
     writeln('Link: https://github.com/lebao3105/Commands-collection');
+    writeln('Exitting...');
+    delay(800);
+end;
+function missing_dir():integer;
+begin
+    textred('Fatal: ');
+    TextColor(White);
+    writeln('Missing directory(_ies) for the program. Exitting...');
+    delay(800);
 end;
 end.
