c73c50c0e991a8224dc91eca2818f2d0fbd612e5
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-02-21 16:31:10 +0700

    All new touch program is here!

--- touch/touch.pas
+++ touch/touch.pas
@@ -1,34 +1,60 @@
 program touch;
-uses
- Sysutils, utils, crt;
-var
-  tfIn: TextFile;
-label createfile, help;
-begin
-   if ParamCount = 1 then begin
-   if ParamStr(1) = 'help' then goto help
-   else begin
-    createfile: begin
-        AssignFile(tfIn, ParamStr(1));
-    // Embed the file handling in a try/except block to handle errors gracefully
-    try
-        rewrite(tfIn);
-        CloseFile(tfIn);
-    except
-        on E: EInOutError do
-        write('File handling error occurred. Details: ', E.Message);
-    end;
 
-  // Wait for the user to end the program
-     writeln('File ', ParamStr(1), ' created.'); exit; end; end; end;
-  if ParamCount = 0 then begin 
-     help: begin
-        textgreen('touch Usage: ');
+uses 
+    {warn,} crt, {color,} 
+    sysutils, strutils;
+
+var 
+    i : integer;
+    ask : string;
+    is_alone : boolean;
+
+procedure check_ask(val,i:string);
+begin
+    if AnsiMatchStr(val, [i]) then 
+        is_alone := true
+    else if AnsiMatchStr(val, ['all']) then
+        is_alone := false
+end;
+
+procedure done(arg:string); // use arg variable for is_alone = true
+begin
+    if is_alone = true then
+        writeln('File ', arg, ' has been created.')
+    else    
+        writeln('All your required file has been created.');
+end;
+
+begin
+    //if ParamCount = 0 then missing_file()
+    {else} if ParamCount > 1 then
+    begin
+        writeln('There are many arguments that we are detected here. Which one you want to create?');
+        writeln('Note that --help flag is not available for this application.');
+        for i := 1 to ParamCount do begin  
+            writeln('Arg no.', i , ' : ', ParamStr(i));
+        end;
+        //textgreen('Enter your answer here (all to use all): ');
+        write('Enter your answer here (all to use all args): ');
         TextColor(White);
-        writeln('touch <file name> : Create <file name> file. This program only can create one file at one time.');
-        writeln('Exiting... Process will exit with code 0 (success).');
-        Delay(800);
-        exit;
-  end;
-  end;
+        read(ask);
+        for i := 1 to ParamCount do begin// we use this loop only when we need it
+            check_ask(ask, IntToStr(i)); // to avoid other crazy loops
+        end;
+        if is_alone = true then begin
+                FileCreate(ParamStr(StrToInt(ask))); 
+        end 
+        else for i := 1 to ParamCount do begin
+                FileCreate(ParamStr(i));
+        end;
+        // check for is_alone again:)
+        if is_alone = true then 
+            done(ParamStr(StrToInt(ask)))
+        else 
+            done('');
+    end
+    else if ParamCount = 1 then begin
+            FileCreate(ParamStr(1));
+            writeln('File ', ParamStr(1), ' has been created.');
+    end;
 end.
