4c26be234fe499387f7b800b5a1a62c675a69c8f
lebao3105 <bao12345yocoo@gmail.com>
2022-03-16 14:22:09 +0700

    file_date program is here

--- file_date/file_date.pas
+++ file_date/file_date.pas
@@ -1,9 +1,29 @@
 program file_date;
+{$mode objFPC}
+
 uses
     sysutils;
+
 var
     i : integer;
+    n : longint;
+
 begin
     if ParamCount = 0 then
+    begin
         writeln('Missing arguments');
+        halt(1);
+    end else begin
+        for i := 1 to ParamCount do
+        begin
+            try
+                n := FileAge(ParamStr(i));
+                writeln('Result: File created on ', DateTimeToStr(FileDateToDateTime(n)));
+            except
+                on E: EInOutError do
+                    writeln('Error occured while reading file: ', E.Message);
+            end;
+            halt(0);
+        end;
+    end;
 end.
