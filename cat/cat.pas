81fedc37c835ed5725484c08400c119d79af6cab
lebao3105 <bao12345yocoo@gmail.com>
2022-06-01 12:09:12 +0700

    Cat verbose tag not fixed yet

--- cat/cat.pas
+++ cat/cat.pas
@@ -1,60 +1,66 @@
-{ verbose flag is not correctly defined yet - the program is not using it. }
 program cat;
 {$mode objFPC}
+
 uses
-   Sysutils, warn, verbose;
+   Sysutils, warn, 
+   verbose, color, crt;
 
 var
    tfIn: TextFile;
    s: string;
-   i: integer;
+   i, k: integer;
 
-label 
-   readfile, help;
+function getFileName(filepath : string): boolean;
+begin
+    if FileExists(filepath) then
+       getFileName := true
+    else
+        textredln('File '+filepath+' not found! Exiting.');
+        halt(-1);
+end;
 
-begin 
-   if ParamCount = 0 then goto help
-   else if ParamCount > 1 then begin
-      if (ParamStr(1) = '-v') and (ParamStr(1) = '--verbose') then goto help;
-      for i := 2 to ParamCount do begin
-          // check for verbose status
-          if (ParamStr(i) = '-v') and (ParamStr(i) = '--verbose') 
-          then begin
-              writeln('Verbose mode turned on.');
-              cat_prog('begin', ParamStr(1));
-          end;
-
-          goto readfile;
-
+function reader(filepath : string): boolean;
+begin
+    getFileName(filepath);
+    assign(tfIn, filepath);
+    try
+      reset(tfIn);
+      while not eof(tfIn) do
+      begin
+        readln(tfIn, s);
+        writeln(s);
       end;
-   end
-   else if ParamCount = 1 then goto readfile;
-
-   // labels
-   help:
-    begin
-        writeln('Usage: cat <filename> <-v/--verbose>');
-        halt(1);
+      CloseFile(tfIn);
+    except
+      on E: EInOutError do
+      begin
+        textredln('File '+filepath+' could not be read!');
+        textredln('Details: '+E.Message);
+        halt(-1);
+      end;
     end;
+end;
 
-   readfile:
+begin
+  if ParamCount = 0 then
+  begin
+      textred('Usage:');
+      TextColor(LightGray);
+      writeln(' cat [file] [-v/--verbose] ...');
+      halt(1);
+  end else if ParamCount = 1 then reader(ParamStr(1))
+  else
     begin
-      Assign(tfIn, ParamStr(1));
-          try
-            reset(tfIn);
-            while not eof(tfIn) do
-            begin
-              readln(tfIn, s);
-              writeln(s);
-            end;
-            CloseFile(tfIn);
-          except
-            on E: EInOutError do begin
-              writeln('Error occured while reading file: ', E.Message);
-              halt(1); end;
-          end;
-      for i := 2 to ParamCount do 
-        if (ParamStr(i) = '-v') and (ParamStr(i) = '--verbose') then
-            cat_prog('end', ParamStr(1));
+      for i := 1 to ParamCount do
+          if (ParamStr(i) = '-v') and (ParamStr(i) = '--verbose') 
+          then
+          begin
+              for k := 1 to ParamCount do
+                if k <> i then 
+                begin
+                    cat_prog('begin', ParamStr(k));
+                    reader(ParamStr(k));
+                end;
+          end else reader(ParamStr(i));
     end;
 end.
--- check_file_type/chk_type.pas
+++ check_file_type/chk_type.pas
@@ -8,26 +8,34 @@ procedure check_type(file_name : String);
 var
     value_type : longint;
 begin
-    if not FileExists(file_name) then
+    if not FileExists(file_name) then 
     begin
-        writeln('File ', file_name, ' does not exist. Aborting.');
-        halt(1);
+        if DirectoryExists(file_name) then
+        begin
+            writeln('This is a directory.');
+            halt(0);
+        end
+        else begin
+            writeln('File ', file_name, ' does not exist. Aborting.');
+            halt(1);
+        end;
     end
     else
     value_type := FileGetAttr(file_name);
     if value_type <> -1 then
         begin
-            write('File ', file_name, ' checked. Here is the result: ');
+            writeln('File ', file_name, ' checked. Here is the result: ');
             if (value_type and faReadOnly) <> 0 then
                 writeln('The file is read only. You should not touch to this file, except this is yours.');
             if (value_type and faHidden) <> 0 then
-                writeln('What a hidden file!');
+                //writeln('What a hidden file!'); // I don't want to have problems because of this
+                writeln('The file is hidden.'); // simply use this
             if (value_type and faSysfile) <> 0 then
                 writeln('This is a system file - you shouldnt touch to it.');
             if (value_type and faVolumeID) <> 0 then
                 writeln('This is a disk label');
             If (value_type and faArchive) <> 0 then
-                Writeln ('File is archive file.');
+                Writeln ('File is a archive.');
             If (value_type and faDirectory) <> 0 then
                 Writeln ('This is a directory. Use dir <item name> to check if a file is a dir/dir is empty istead of using this program.');
         end
