1aa65aa24eab7896398a2c7c7e480fa098d06f38
lebao3105 <bao12345yocoo@gmail.com>
2022-07-09 16:31:51 +0700

    Finally fixed all listing problems

--- dir/listing.pas
+++ dir/listing.pas
@@ -17,25 +17,33 @@ Implementation
 procedure listitems(dir:string);
 begin
     if DirectoryExists(dir) then
-        if FindFirst(dir+'*', faAnyFile, f) then
+    begin
+        if FindFirst(dir + '/*', faAnyFile, f) = 0 then
         begin
             repeat
-                Inc(l);   
+                Inc(l);  
+                FindNext(f); 
                 with f do
-                    writeln(Name:40, Size:15);
+                begin
+                    writeln(Name:20, Size:15);
+                end;
             until FindNext(f) <> 0;
-            textgreen('Found '+ l + 'items in '+ dir+ ' directory.');
-            exit(0);
+            FindClose(f);
+            textgreenln('Found '+ IntToStr(l) + ' items in '+ dir+ ' directory.');
+            TextColor(White);
+            writeln('Done!');
+            halt(0);
         end
         
         else begin
             textred('Unable to open directory '+ dir+ 'yet.');
             TextColor(White);
-            exit(1);
+            halt(1);
         end;
+    end
     else
         not_a_dir(dir);
-        exit(1);
+        halt(1);
 end;
 
 end.
