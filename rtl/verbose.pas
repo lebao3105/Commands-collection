b8004e2f7f2c1445e884230d2d058b2f76cb1b0d
lebao3105 <bao12345yocoo@gmail.com>
2022-07-09 16:00:23 +0700

    Fix typos

--- rtl/verbose.pas
+++ rtl/verbose.pas
@@ -12,18 +12,19 @@ implementation
 procedure dir_prog(s, dir:string);
 begin
     case s of
-    'begin': begin textgreenln('Going to open directory ', dir); TextColor(White); end;
-    'end' : begin textgreenln('Done opening and listing its content of ', dir, ' directory.'); TextColor(White); end;
+        'begin': begin textgreenln('Going to open directory '+ dir); TextColor(White); end;
+        'end' : begin textgreenln('Done opening and listing its content of '+ dir+ ' directory.'); TextColor(White); end;
+    end;
 end;
 
 procedure cat_prog(s, file_name:string);
 begin
     if s = 'begin' then
-        textgreenln('cat is going to read the content of file: ', file_name);
+        textgreenln('cat is going to read the content of file: '+ file_name);
   		textgreenln('=========================================');
         TextColor(White);
     if s = 'end' then
-        textgreenln('Done reading file name ', file_name);
+        textgreenln('Done reading file name '+ file_name);
         TextColor(White);
 end;    
 
