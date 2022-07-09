934464d58ccea06b703f205e046934495092714a
lebao3105 <bao12345yocoo@gmail.com>
2022-07-09 15:56:47 +0700

    "Verbose" procedure for dir program

--- rtl/verbose.pas
+++ rtl/verbose.pas
@@ -1,18 +1,30 @@
 unit verbose;
 // print verbose message
 interface
-    
+
+uses color, crt;
+
+procedure dir_prog(s, dir:string);
 procedure cat_prog(s, file_name:string);
 
 implementation
 
+procedure dir_prog(s, dir:string);
+begin
+    case s of
+    'begin': begin textgreenln('Going to open directory ', dir); TextColor(White); end;
+    'end' : begin textgreenln('Done opening and listing its content of ', dir, ' directory.'); TextColor(White); end;
+end;
+
 procedure cat_prog(s, file_name:string);
 begin
     if s = 'begin' then
-        writeln('cat is going to read the contents of file: ', file_name);
-  		writeln('=========================================');
+        textgreenln('cat is going to read the content of file: ', file_name);
+  		textgreenln('=========================================');
+        TextColor(White);
     if s = 'end' then
-        writeln('cat has read the contents of file: ', file_name);
-        writeln('Press Enter to exit the program.');
+        textgreenln('Done reading file name ', file_name);
+        TextColor(White);
 end;    
+
 end.
