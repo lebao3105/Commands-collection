8ff9edf044785cb955fd06a8383c10e70d8dd191
lebao3105 <bao12345yocoo@gmail.com>
2022-07-09 15:43:18 +0700

    Update warn.pas
    Make all functions to procedures, and write a new procedure

--- rtl/warn.pas
+++ rtl/warn.pas
@@ -1,48 +1,57 @@
 // Unit to warn the user about something e.g missing file
 unit warn;
 Interface
-function missing_num():integer;
-function missing_argv():integer;
-function missing_file():integer;
-function help_prog():integer;
-function missing_dir():integer;
+procedure missing_num();
+procedure missing_argv();
+procedure missing_file();
+procedure help_prog();
+procedure missing_dir();
+procedure not_a_dir(dir:string);
 Implementation
 uses
     color, crt;
 
-function missing_num():integer;
+procedure missing_num();
 begin
     textred('Fatal: ');
     TextColor(White);
     writeln('You are missing number(s) to do this action. Exitting...');
     delay(800);
 end;
-function missing_argv():integer;
+procedure missing_argv();
 begin
-    textred('Fatal: ');
+    textred('Error: ');
     TextColor(White);
     writeln('You are missing (an) argument(s) for the application. Exitting...');
     delay(800);
 end;
-function missing_file():integer;
+procedure missing_file();
 begin
     textred('Fatal: ');
     TextColor(White);
     writeln('The program needs (one) more file(s) to start. Exitting...');
     delay(800);
 end;
-function help_prog():integer;
+procedure help_prog();
 begin
     writeln('You may want to use the help program or read the README.md file from the source code.');
     writeln('Link: https://github.com/lebao3105/Commands-collection');
     writeln('Exitting...');
     delay(800);
 end;
-function missing_dir():integer;
+procedure missing_dir();
 begin
     textred('Fatal: ');
     TextColor(White);
     writeln('Missing directory(_ies) for the program. Exitting...');
+    delay(800);
+end;
+procedure not_a_dir(dir:string);
+begin
+    textred('Error occured: ');
+    TextColor(White);
+    writeln('Target item ', dir, ' is not exist, or is not a directory yet.');
+    writeln('Check your input and try again.');
     delay(800);
 end;
 end.
