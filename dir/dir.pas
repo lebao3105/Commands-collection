5781ef0839fe2d8da79cd6e9b86c50594226e68a
lebao3105 <bao12345yocoo@gmail.com>
2022-07-11 16:49:39 +0700

    Allow to use dir with no other parameter(s)

--- dir/dir.pas
+++ dir/dir.pas
@@ -7,12 +7,7 @@ var i: integer;
 
 begin
   if ParamCount = 0 then
-  begin
-    writeln('Usage: dir [directory]');
-    missing_argv();
-    halt(1);
-  end
-  
+  	listitems('.')
   else if ParamCount > 1 then
   begin
       for i := 1 to ParamCount do
