613e0198b66f9b3016bc60323390c32f98b6eaa4
lebao3105 <bao12345yocoo@gmail.com>
2022-07-21 15:14:31 +0700

    Only do 1 function - that's all.

--- pwd/pwd.pas
+++ pwd/pwd.pas
@@ -1,18 +1,9 @@
-program pwd; // program to show the current directory
+program pwd;
 uses 
     sysutils, warn;
 var 
     n : integer;
+
 begin
-    if ParamCount > 0 then
-    for n := 1 to ParamCount do begin
-        if ParamStr(n) = '--verbose' then
-            writeln('pwd shows the current directory. And now its output is...')
-        else if ParamStr(n) = '' then
-            break
-        else if ParamStr(n) = '--help' then
-            help_prog();
-    end;
-        writeln(GetCurrentDir); // that's it
-    exit;
+    writeln(GetCurrentDir); 
 end.
