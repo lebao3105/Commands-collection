20b6b392899194acb5f38a73d9521c6433243832
lebao3105 <bao12345yocoo@gmail.com>
2022-07-30 14:15:31 +0700

    Last file modification time & check symlink

--- check_file_type/chk_type.pas
+++ check_file_type/chk_type.pas
@@ -4,6 +4,18 @@ uses
 var
     n : integer;
 
+procedure get_date(filepath:string);
+// referenced from freepascal's wiki
+var
+    s : TDateTime;
+    fa : longint;
+begin
+    fa := FileAge(filepath);
+    if fa <> -1 then
+        s := FileDateToDateTime(fa);
+        writeln('Last modification time: ', DateTimeToStr(s));
+end;
+
 procedure check_type(file_name : String);
 var
     value_type : longint;
@@ -25,6 +37,7 @@ begin
     if value_type <> -1 then
         begin
             writeln('File ', file_name, ' checked. Here is the result: ');
+            get_date(file_name);
             if (value_type and faReadOnly) <> 0 then
                 writeln('The file is read only. You should not touch to this file, except this is yours.');
             if (value_type and faHidden) <> 0 then
@@ -32,20 +45,21 @@ begin
                 writeln('The file is hidden.'); // simply use this
             if (value_type and faSysfile) <> 0 then
                 writeln('This is a system file - you shouldnt touch to it.');
-            if (value_type and faVolumeID) <> 0 then
-                writeln('This is a disk label');
+            if (value_type and faSymLink) <> 0 then
+                writeln('The file is a symlink to another item');
             If (value_type and faArchive) <> 0 then
                 Writeln ('File is a archive.');
             If (value_type and faDirectory) <> 0 then
-                Writeln ('This is a directory. Use dir <item name> to check if a file is a dir/dir is empty istead of using this program.');
+                Writeln ('This is a directory.');
         end
-        else writeln('An error occured while checking the file!');
+    else writeln('An error occured while checking the file!');
 end;
 
 begin
     if ParamCount = 0 then
         writeln('Missing argument. Exiting.')
-    else for n := 1 to ParamCount do begin
-        check_type(ParamStr(n));
-    end;
+    else
+        for n := 1 to ParamCount do begin
+            check_type(ParamStr(n));
+        end;
 end.
