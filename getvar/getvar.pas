57efb43f7550280aca4b2db3156d0de24f68963c
lebao3105 <bao12345yocoo@gmail.com>
2022-08-06 15:22:55 +0700

    Update getvar

--- getvar/getvar.pas
+++ getvar/getvar.pas
@@ -9,28 +9,37 @@ var i : longint;
     n : integer;
 
 begin
-  if ParamCount = 0 then  
-      for i:=1 to EnvCount do
+  if ParamCount = 0 then begin
+    for i := 1 to EnvCount do
         WriteLn(EnvStr(i));
+    textgreen('Done ');
+    TextColor(LightGray);
+    writeln('showing ALL defined variables on the system. Use --help parameter to see examples.');
+  end;
 
-  if ParamCount >=1 then begin
-      for n :=1 to ParamCount do
-          writeln(GetEnv(ParamStr(n)));
-
-      // but what if the user run this program on Linux with USERPROFILE?
-      if ParamStr(n) = 'USERPROFILE' then 
-        {$IFDEF UNIX}
-        GetEnv('HOME');
-        {$ENDIF} 
+  if ParamCount >= 1 then
+    for n := 1 to ParamCount do begin
+      // what if the user run this program on Linux with USERPROFILE?
+      {$IFDEF UNIX}
+      if ParamStr(n) = 'USERPROFILE' then
+        writeln(GetEnv('HOME'));
+      {$ENDIF} 
         
-      if ParamStr(n) = '--help' then
-      begin
+       if ParamStr(n) = '--help' then
+       begin
           writeLn('Use getvar with anything you want to get its value.');
           writeln('If you run this application without any arguments, getvar will');
           writeln('list all available variables.');
           writeln('On UNIX, you can use USERPROFILE variable!');
-      end;
-  end;
+          writeln('Examples:');
+          writeln(ParamStr(0) , ' windir will return C:\Windows on Windows.');
+          writeln(ParamStr(0), ' path will return the system''s path.');
+       end
+
+       else
+         writeln(GetEnv(ParamStr(n)));
+    end;
+  halt(0);
 end.
 
 
