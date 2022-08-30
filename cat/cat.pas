4dfeeac882306b4af5b4e568a27f07dfbab78608
lebao3105 <bao12345yocoo@gmail.com>
2022-08-30 22:19:40 +0700

    Cat working again!

--- cat/cat.pas
+++ cat/cat.pas
@@ -2,58 +2,64 @@ program cat;
 {$mode objFPC}
 
 uses
-   Sysutils, warn, 
-   verbose, color, crt;
+	sysutils, warn, 
+	verbose, color, crt;
 
 var
-   tfIn: TextFile;
-   s: string;
-   i, k: integer;
+	tfIn: TextFile;
+	s: string;
+	file_content: string;
+	i, k: integer;
 
-function getFileName(filepath : string): boolean;
-begin
-    if FileExists(filepath) then
-       getFileName := true
-    else
-        textredln('File '+filepath+' not found! Exiting.');
-        TextColor(LightGray);
-		getFileName := false;
-        halt(-1);
-end;
-
-function reader(filepath : string): boolean;
-var checkResult: boolean;
-begin
-  checkResult := getFileName(filepath);
-  if (checkResult = true) then begin
-    assignFile(tfIn, filepath);
-	reset(tfIn);
-    try
-      while not eof(tfIn) do begin
-		readln(tfIn, s);
-		writeln(s);
-	  end;
-      CloseFile(tfIn);
-    except
-      on E: EInOutError do
-      begin
-        textredln('File '+filepath+' could not be read!');
-        textredln('Details: '+E.Message);
-        halt(-1);
-      end;
-    end;
-  end;
-end;
+label
+	check, readfile;
 
 begin
-  if ParamCount = 0 then
-  begin
-      textred('Usage:');
-      TextColor(LightGray);
-      writeln(' cat [file]...');
-      halt(1);
-  end
-  else
-    for i := 1 to ParamCount do
-        reader(ParamStr(i));
+	if ParamCount = 0 then
+	begin
+		textred('Usage: ');
+		TextColor(LightGray);
+		writeln(ParamStr(0), ' [file]');
+		missing_argv;
+		halt(1);
+	end
+
+	else begin
+		for i := 1 to ParamCount do
+			s := ParamStr(i);
+			goto readfile;
+		halt(0);
+	end;
+
+	check: begin
+		if FileExists(s) then begin
+			cat_prog(s, 'begin');
+			goto readfile;
+		end
+		else begin
+			textredln('File '+s+' not found!');
+			TextColor(LightGray);
+			if ParamCount = 0 then
+				writeln('Exiting now.');
+				halt(-1);
+		end;
+	end;
+
+	readfile: begin
+		assignFile(tfIn, s);
+		reset(tfIn);
+		try
+			while not EOF(tfIn) do begin
+				readln(tfIn, file_content);
+				writeln(file_content);
+			end;
+			CloseFile(tfIn);
+			cat_prog(s, 'end');
+		except
+			on E: EInOutError do begin
+				textredln('Error occured while reading file '+s+': '+E.Message);
+				halt(1);
+			end;
+		end;
+	end;
 end.
