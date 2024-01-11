5575e1cf6eaf69b6ea5662857bc63153014d1b47
lebao3105 <bao12345yocoo@gmail.com>
2024-01-11 12:03:58 +0700

    Merge calldate into calltime

--- calltime/calltime.pas
+++ calltime/calltime.pas
@@ -2,53 +2,43 @@ program calltime;
 {$mode objFPC}
 
 uses
-    classes, custapp, dos;
+    classes, custapp, dos, sysutils;
 
 type TCallTime = class(TCustomApplication)
 protected
     procedure DoRun; override;
 end;
 
-var
-    CallTimeApp: TCallTime;
-    hr, min, sec, msec: word;
-
-// if the time is < 10, add a 0 before it
-// e.g 6 hours < 10 hours, so this function will return 06 (hours)
-// function taken from freepascal wiki
-function addzero(w:word):string;
-var
-    s:string;
-begin
-    Str(w, s);
-    if w < 10 then
-        addzero := '0' + s
-    else
-        addzero := s;
-end;
-
 procedure TCallTime.DoRun;
 var
     errorMsg: string;
+    ThisMonement: TDateTime;
+    format: string = 'dddd mmmm dd yyyy tt';
 begin
-    errorMsg := CheckOptions('h', ['help', 'no-msec']);
+    errorMsg := CheckOptions('hnf:', ['help', 'number-only', 'format:']);
     if errorMsg <> '' then begin writeln(errorMsg); halt(1); end;
 
     if HasOption('h', 'help') then
     begin
         writeln(ParamStr(0), ' [flag]');
         writeln('Use --help/-h flag to show this message (again).');
-        writeln('--no-msec to disable showing miliseconds.');
+        writeln('-f / --format sets the custom format to be used');
+        writeln('Available format strings can be found at https://www.freepascal.org/docs-html/rtl/sysutils/formatchars.html');
         halt(0);
     end;
 
-    write('The current time is: ', addzero(hr) + ':' + addzero(min) + ':' + addzero(sec));
-    if not HasOption('no-hsec') then
-        writeln(addzero(msec));
+    if HasOption('f', 'format') then format := GetOptionValue('f', 'format');
+
+    ThisMonement := Now;
+
+    writeln('The current time is: ', FormatDateTime(format, ThisMonement));
+    Terminate;
 end;
 
+var
+    CallTimeApp: TCallTime;
+
 begin
-    GetTime(hr, min, sec, msec);
     CallTimeApp := TCallTime.Create(nil);
     CallTimeApp.StopOnException := true;
     CallTimeApp.Run;
