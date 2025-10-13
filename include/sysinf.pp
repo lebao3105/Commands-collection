3a3a89ea0485ae35f97556b93a73e699a3992ac2
lebao3105 <bao12345yocoo@gmail.com>
2025-10-13 22:09:15 +0700

    Fix typos

--- include/sysinf.pp
+++ include/sysinf.pp
@@ -12,7 +12,7 @@ uses base, ctypes;
 type
 
     RSysInfo = record
-        uptime: long; { Secons since boot }
+        uptime: long; { seconds since boot }
         loads: array[0..2] of ulong; { 1, 5, 15 minute load averages }
         totalram: ulong; { total usable main memory size }
         freeram: ulong; { total free main memory size }
@@ -20,7 +20,7 @@ type
         bufferram: ulong; { memory used by buffers }
         totalswap: ulong; { total swap space size }
         freeswap: ulong; { swap space still available }
-        procs: uint16; { number of current processes}
+        procs: uint16; { number of current processes }
         totalhigh: ulong; { total high memory size }
         freehigh: ulong; { available high memory size }
         mem_unit: uint32; { memory unit size in bytes }
