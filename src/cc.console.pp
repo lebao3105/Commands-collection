7e895a20af4d17f0bcae6a3bb220c52c78f44a13
lebao3105 <bao12345yocoo@gmail.com>
2026-04-18 16:12:56 +0700

    refactor: Use RTL's crt and remove unneccessary(?) tasks

--- src/cc.console.pp
+++ src/cc.console.pp
@@ -7,24 +7,20 @@ uses
     unixapi.base,
     unixapi.termio,
     system.sysutils,
-    system.console.keyboard,
+    system.console.crt,
+    system.console.keyboard
     {$else}
     baseunix,
     termio,
     sysutils,
-    keyboard,
+    crt,
+    keyboard
     {$endif}
-    cc.logging
     ;
 
 var
-    originalOuts: array[0..1] of termios;
-    // 0st index: stdout
-    // 1st index: stderr
-    originalStdIn: termios;
     NeedToRefreshSz: bool = true;
     Sz: WinSize;
-    modifiedStdIn: bool;
 
 fn isATerminal(Handle: cint): bool;
 begin
@@ -67,64 +63,15 @@ begin
     end;
 end;
 
-retn modifyOutputHandle_Internal(for_stderr: bool);
-var
-    indx: int;
-    h: cint;
-    modi: termios;
-{$push} {$warn 5057 off} // Local variable seems to be not initialized
-begin
-    if for_stderr then begin
-        indx := 1;
-        h := StdErrorHandle;
-    end
-    else begin
-        indx := 0;
-        h := StdOutputHandle;
-    end;
-
-    if not isATerminal(h) then
-        return;
-
-    if tcgetattr(h, originalOuts[indx]) = -1 then
-        return; // TODO: Do something
-
-    modi := originalOuts[indx];
-    modi.c_oflag := modi.c_oflag or ONLCR; // (XSI) Map NL to CRNL
-    modi.c_oflag := modi.c_oflag or ONOCR; // Don't output CR at column 0
-    modi.c_oflag := modi.c_oflag and not OCRNL; // Don't map CR to NL
-
-    if tcsetattr(h, TCSANOW, modi) = -1 then
-        return; // TODO: Do something
-end;
-{$pop}
-
-retn restoreStdOut_Internal(for_stderr: bool);
-var
-    indx: int;
-    h: cint;
-begin
-    if for_stderr then begin
-        indx := 1;
-        h := StdErrorHandle;
-    end
-    else begin
-        indx := 0;
-        h := StdOutputHandle;
-    end;
-
-    if not isATerminal(h) then
-        return;
-    
-    if tcsetattr(h, TCSANOW, originalOuts[indx]) = -1 then
-        return; // TODO: Do something
-end;
-
 retn screenClear;
 begin
     if not isATerminal(OutputHandle) then
         return;
 
+    // The thing is that not all OSes support this, e.g Windows older than 1607.
+    // UNIXes normally should cover this themselves. RTL's crt has a function
+    // that does a *bit* more than just printing these ASCII keys, but this is
+    // enough for us, I think.
     write(OutputFile, ESC_KEY+'[2J');
     write(OutputFile, ESC_KEY+'[H'); // move the cursor to the top-left corner
 end;
@@ -138,7 +85,7 @@ end;
 
 fn enableRawStdIn(disableCtrlCZ: bool): bool;
 var modi: termios;
-{$push} {$warn 5057 off} // Local variable seems to be not initialized
+{$push} {$warn 5036 off} // Local variable seems to be not initialized
 begin
     if tcgetattr(StdInputHandle, modi) = -1 then
         return(false);
@@ -158,30 +105,26 @@ begin
     modi.c_cc[VTIME] := 1; // maximum amount of time to wait, in 1/10ths of a second
 
     Result := tcsetattr(StdInputHandle, TCSAFLUSH, modi) <> -1;
-    modifiedStdIn := Result;
 end;
 {$pop}
 
 fn disableRawStdIn: bool;
 var modi: termios;
-{$push} {$warn 5057 off} // Local variable seems to be not initialized
+{$push} {$warn 5036 off} // Local variable seems to be not initialized
 begin
     if tcgetattr(StdInputHandle, modi) = -1 then
         return(false);
     
     modi.c_lflag := modi.c_lflag or (ICANON or IEXTEN or ISIG);
     modi.c_iflag := modi.c_iflag or (IXON or ICRNL);
-    modi.c_cc[VMIN] := originalStdin.c_cc[VMIN]; // FIXME: What if originalStdIn is empty?
-    modi.c_cc[VTIME] := originalStdin.c_cc[VTIME];
 
-    Result := tcsetattr(StdInputHandle, TCSANOW, originalStdIn) <> -1;
-    modifiedStdIn := not Result;
+    Result := tcsetattr(StdInputHandle, TCSAFLUSH, modi) <> -1;
 end;
 {$pop}
 
 fn enableStdInEchoing(enable: bool): bool;
 var modi: termios;
-{$push} {$warn 5057 off} // Local variable seems to be not initialized
+{$push} {$warn 5036 off} // Local variable seems to be not initialized
 begin
     if tcgetattr(StdInputHandle, modi) = -1 then
         return(false);
@@ -191,12 +134,12 @@ begin
     else
         modi.c_lflag := modi.c_lflag and not ECHO;
 
-    Result := tcsetattr(StdInputHandle, TCSANOW, modi) <> -1;
-    modifiedStdIn := Result;
+    Result := tcsetattr(StdInputHandle, TCSAFLUSH, modi) <> -1;
 end;
 {$pop}
 
 fn stdInReadKey: string;
+{$push}{$warn 4104 off} // implicit ansi -> unicode type conversion
 var K: TKeyEvent;
 begin
     K := TranslateKeyEvent(GetKeyEvent);
@@ -212,39 +155,24 @@ begin
             Result := ShiftStateToString(K, true) + '-' + Result;
     end;
 end;
+{$pop}
 
-{$push} {$warn 5058 off} // Local variable seems to be not initialized
 initialization
 
-Debug('Initializing console module...', []);
-
-modifyOutputHandle_Internal({ for stderr }true);
-modifyOutputHandle_Internal(false);
-
-// This one modifies console attributes. Took a good amount of time
-// just to figure it out (I just stumbled upon putting this on
-// a reproduction).
-// As a result, this call lies here. Also it's to make sure that calls of
-// stdin modification functions use modified attributes.
-// Wait. We must back it up first!
-if tcgetattr(StdInputHandle, originalStdIn) = -1 then
-    Error('Unable to backup standard input attributes. Expect weird behavior.', []);
-InitKeyboard;
-
+{ Avoid funny outputs
+  Like this
+             simple 
+                    example}
 SetTextLineEnding(stderr, #13#10);
 SetTextLineEnding(stdout, #13#10);
 
-finalization
+InitKeyboard;
 
-Debug('Restoring default console attributes...', []);
-
-DoneKeyboard;
-
-if modifiedStdIn and (tcsetattr(StdInputHandle, TCSANOW, originalStdIn) = -1) then
-    Error('Failed to restore standard input attributes.', []);
-
-restoreStdOut_Internal({ for stderr }true);
-restoreStdOut_Internal(false);
+{ Modify unit CRT's settings }
+{ Unused? }
+{
+    CheckBreak := true;
+    CheckEOF   := true;
+}
 
 end.
-{$pop}
