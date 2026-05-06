d7eaa6feb1690937f5946f2e13394f8ef1d6a4e9
lebao3105 <bao12345yocoo@gmail.com>
2026-05-06 21:40:19 +0700

    feat: argument pairs

--- src/touch/touch.pp
+++ src/touch/touch.pp
@@ -1,24 +1,52 @@
 program touch;
 {$modeswitch anonymousfunctions}
+{$modeswitch result}
+// {$IOChecks OFF}
 
 uses
     {$ifdef FPC_DOTTEDUNITS}
     system.sysutils,
     system.strutils,
+    system.types,
     {$else}
     sysutils,
     strutils,
+    types,
     {$endif}
     cc.base,
     cc.fs,
     cc.logging,
-    cc.getopts
+    cc.getopts,
+    i18n
     ;
 
 {$I i18n.inc}
 
 var
     beVerbose, createParent, dirsOnly: boolean;
+
+retn CreateFolder(path: string); forward;
+
+retn CreateParentFolder(fullPath: string);
+var
+    currentPath: string;
+    idx: int;
+    splits: TStringDynArray;
+begin
+    // NOTE: RTL's systutils has ForceDirectories that does the same thing.
+    if not createParent then return;
+
+    splits := SplitString(fullPath, DirectorySeparator);
+    currentPath := '';
+
+    for idx := Low(splits) to High(splits) - 1 do
+    begin
+        currentPath := specialize IfThenElse<string>(
+            idx > 0, ConcatPaths([currentPath, splits[idx]]), splits[idx]
+        ); // hmmmmm
+        CreateFolder(currentPath);
+    end;
+end;
 
 retn CreateFolder(path: string);
 begin
@@ -31,11 +59,6 @@ begin
     {$undef IS_MKDIR}
     {$I makething.inc}
 end;
-
-var
-    currentPath: string;
-    splits: array of ansistring;
-    j, i: integer;
 
 begin
     if ParamCount = 0 then
@@ -51,24 +74,35 @@ begin
     end;
     cc.getopts.GetOpt;
 
-    for i := 0 to High(cc.getopts.NonOpts) do begin
-        if createParent then
-        begin
-            splits := SplitString(cc.getopts.NonOpts[i], DirectorySeparator);
-
-            for j := Low(splits) to High(splits) - 1 do
+    if cc.getopts.OptHasPairs then
+        specialize ArrayForEach<TStringDynArray>(
+            cc.getopts.GetArgPairs,
+            fn (arg: TStringDynArray): bool
             begin
-                currentPath := specialize IfThenElse<string>(
-                    j > Low(splits), ConcatPaths([currentPath, splits[j]]), splits[j]
-                ); // hmmmmm
+                if arg[1] = 'd' then begin
+                    CreateParentFolder(arg[0]);
+                    CreateFolder(arg[0]);
+                end
+                else if arg[1] = 'f' then begin
+                    CreateParentFolder(arg[0]);
+                    CreateFile(arg[0]);
+                end
+                else
+                    Warning(INVALID_TYPE, [ arg[0], arg[1] ]);
+                return(false);
+            end
+        )
+    else
+        specialize ArrayForEach<string>(
+            cc.getopts.NonOpts,
+            fn (arg: string): bool
+            begin
+                if dirsOnly then
+                    CreateFolder(arg)
+                else
+                    CreateFile(arg);
 
-                CreateFolder(currentPath);
-            end;
-        end;
-
-        if dirsOnly then
-            CreateFolder(cc.getopts.NonOpts[i])
-        else
-            CreateFile(cc.getopts.NonOpts[i]);
-    end;
+                return(false);
+            end
+        );
 end.
