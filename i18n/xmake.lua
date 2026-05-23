78b9249d1d132bd318a6222ef2f6009b82d0c67b
lebao3105 <bao12345yocoo@gmail.com>
2026-05-23 18:31:39 +0700

    i18n: Update strings

--- i18n/xmake.lua
+++ i18n/xmake.lua
@@ -1,8 +1,8 @@
 target("API-i18n")
     set_kind("phony")
 
-    on_build(function (target)
-        local args = { "-dPASDOC", "@cc.cfg", miscs.get_custom_fpc_conf() }
+    on_build( function (target)
+        local args = { "-dNO_PROG", "@cc.cfg", miscs.get_custom_fpc_conf() }
 
         for __, fullpath in ipairs(os.files("src/shared/cc.*.pp")) do
             -- TODO: Fix this terrible hardcoded FPC executable
@@ -11,7 +11,7 @@ target("API-i18n")
         end
     end)
 
-    after_build(function (target)
+    after_build( function (target)
         i18n.generate_pot("src/shared", "i18n/cc.pot", false)
 
         for _, fullpath in ipairs(os.dirs("i18n/*")) do
@@ -21,7 +21,7 @@ target("API-i18n")
         end
     end)
 
-    on_clean(function (_)
+    on_clean( function (_)
         os.rm("i18n/*/cc.mo")
         os.rm("src/shared/*.o")
         os.rm("src/shared/*.ppu")
