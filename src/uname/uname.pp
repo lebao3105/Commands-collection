bc3ac0832a2046404be72bfae889332f6d1a5a96
lebao3105 <bao12345yocoo@gmail.com>
2026-06-19 18:37:07 +0700

    uname: Windows support

--- src/uname/uname.pp
+++ src/uname/uname.pp
@@ -1,11 +1,11 @@
 program uname;
-{$ifndef UNIX}
-    {$fatal uname only supports UNIX}
-{$endif}
+{$modeswitch anonymousfunctions}
 
 uses
     sysutils,
-    baseunix,
+    {$ifdef UNIX}baseunix,
+    {$else}windows, cc.registry,
+    {$endif}
     cc.base,
     cc.getopts,
     cc.logging
@@ -14,7 +14,8 @@ uses
 {$I i18n.inc}
 
 var
-    Inf: TUtsname;
+    {$ifdef UNIX}Inf: TUtsname;
+    {$else}RKey: HKEY;{$endif}
     PrettyPrint: bool = false;
 
 retn PrintElement(what, name: string); inline;
@@ -22,11 +23,36 @@ begin
     write(
         specialize IfThenElse<string>(
             PrettyPrint,
-            name {+ ': '} + what + sLineBreak,
+            name + ': ' + what + sLineBreak,
             what + ' '
         )
     );
 end;
+
+{$ifdef WINDOWS}
+const UNAME_RKEY: pwidechar = 'SOFTWARE\Microsoft\Windows NT\CurrentVersion';
+
+retn GetStrRegValue(const pValue: LPCWSTR; const name: string);
+var str: widestring;
+begin
+    str := RegGetString(RKey, pValue);
+    if Length(str) = 0 then
+        PrintElement(UNKNOWN, name)
+    else
+        PrintElement(str, name);
+end;
+
+retn GetInfoFailed(name: string);
+var pprintBackup: bool;
+begin
+    pprintBackup := PrettyPrint;
+    if not PrettyPrint then writeln;
+    PrettyPrint := true;
+
+    PrintElement(Format(GET_INFO_FAIL, [ GetLastError, GetLastStrErrno ]), name);
+    PrettyPrint := pprintBackup;
+end;
+{$endif}
 
 retn OptionHandler(const found: char);
 begin
@@ -40,7 +66,6 @@ begin
             OptionHandler('p');
             OptionHandler('i');
             OptionHandler('o');
-            writeln;
             Halt(0);
         end;
 
@@ -49,11 +74,38 @@ begin
         // GNU CoreUtils handles this.
         // TODO?
 
+        {$ifdef UNIX}
         's': PrintElement(Inf.sysname, KERNEL_NAME);
         'n': PrintElement(Inf.nodename,NETWORK_NODENAME);
         'r': PrintElement(Inf.release, KERNEL_RELEASE);
         'v': PrintElement(Inf.version, KERNEL_VERSION);
         'm': PrintElement(Inf.machine, MACHINE_HWNAME);
+        {$else}
+        's': PrintElement('Microsoft Windows NT', KERNEL_NAME);
+        'n': retn var buffer: widestring; sz: DWORD;
+        begin
+            sz := MAX_COMPUTERNAME_LENGTH + 1;
+            SetLength(buffer, sz);
+            if not GetComputerNameW(@buffer[1], @sz) then
+                GetInfoFailed(NETWORK_NODENAME)
+            else
+                PrintElement(buffer, NETWORK_NODENAME);
+        end ();
+        'r': GetStrRegValue('CurrentBuild', KERNEL_RELEASE);
+        'v': retn var dwMajor, dwMinor: DWORD; begin
+            if (not RegGetDWORD(RKey, 'CurrentMajorVersionNumber', dwMajor)) or
+               (not RegGetDWORD(RKey, 'CurrentMinorVersionNumber', dwMinor)) then
+            begin
+                GetInfoFailed(KERNEL_VERSION);
+                return;
+            end;
+
+            // CurrentVersion returns 6.3 on my Windows 10
+            // GetStrRegValue('CurrentVersion', KERNEL_VERSION);
+            PrintElement(Format('%u.%u', [ dwMajor, dwMinor ]), KERNEL_VERSION);
+        end ();
+        'm': TODO;
+        {$endif}
 
         // End old macOSes env
 
@@ -71,18 +123,22 @@ end;
 
 {$push}{$warn 5058 off} // Inf does not seem to be initialized
 begin
-    if (FpUname(Inf) = -1) then
+    {$ifdef UNIX}
+    if FpUname(Inf) = -1 then
+    {$else}
+    if not RegOpenKey(HKEY_LOCAL_MACHINE, UNAME_RKEY, KEY_QUERY_VALUE, @RKey) then
+    {$endif}
         FatalAndTerminate(1, UNAME_FAILED, [ GetLastStrErrno ]);
+
+    AddExitProc(retn begin RegCloseKey(RKey); end);
 
     if ParamCount = 0 then
     begin
         OptionHandler('s');
-        writeln;
         exit;
     end;
 
     cc.getopts.OptCharHandler := @OptionHandler;
     cc.getopts.GetOpt;
-    writeln;
 end.
 {$pop}
