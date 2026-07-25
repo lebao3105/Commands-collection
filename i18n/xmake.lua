53c5f386cc28b69e646b2de4f3e962fdfa0bbbb1
lebao3105 <bao12345yocoo@gmail.com>
2026-07-26 00:16:43 +0700

    i18n: Path changes+build script refactor

--- i18n/xmake.lua
+++ i18n/xmake.lua
@@ -12,7 +12,7 @@ task("i18n")
                                      "    - clean" },
             { nil, "what", "v", nil, "What to localize for",
               values = table.join(programs, {
-                  "all", "API", "programs"
+                  "all"
               }) }
         }
     }
@@ -25,44 +25,44 @@ task("i18n")
         local what_to_do = option.get("task")
         local what_for = option.get("what")
 
-        if what_for == "programs" then
-            -- The use of os.dirs(...) is intentional
-            for _, p in ipairs(os.dirs(os.projectdir() .. "/src/*")) do
-                local name = path.filename(p)
-                if os.isfile(p .. '/' .. name .. ".pp") then
-                    task.run("i18n", { task = what_to_do, what = name })
+        if what_for == "all" then
+            if what_to_do == "clean" then
+                for _, path in ipairs({ "**/*.mo" }) do
+                    os.rm(path)
+                end
+            else
+                -- The use of os.dirs(...) is intentional
+                for _, p in ipairs(os.dirs(os.projectdir() .. "/src/*")) do
+                    local name = path.filename(p)
+                    if os.isfile(p .. '/' .. name .. ".pp") then
+                        task.run("i18n", { task = what_to_do, what = name })
+                    end
                 end
             end
             return
-        elseif what_for == "all" then
-            task.run("i18n", { task = what_to_do, what = "programs" })
-            task.run("i18n", { task = what_to_do, what = "API" })
+        elseif what_to_do == "all" then
+            task.run("i18n", { task = "pot", what = what_for })
+            task.run("i18n", { task = "po", what = what_for })
+            task.run("i18n", { task = "mo", what = what_for })
             return
-        end
-
-        if not miscs.is_string_empty(what_for) then
-            if what_to_do == "clean" then
-                print(what_for .. " is not needed for xmake i18n clean")
-            elseif what_to_do == "all" then
-                task.run("i18n", { task = "pot", what = what_for })
-                task.run("i18n", { task = "po", what = what_for })
-                task.run("i18n", { task = "mo", what = what_for })
-                return
-            end
-        else
+        elseif miscs.is_string_empty(what_for) then
             os.exec("xmake i18n --help")
             raise("[what] is required here. Exiting.")
         end
 
-        import("i18n")
         import("core.project.config")
+        import("private.action.require.impl.package")
         config.load() -- to make core.project.config.get() work
 
-        local i18n_dir = "i18n/"
-        if what_for ~= "API" then
-            i18n_dir = "src/" .. what_for .. "/i18n/"
+        local gettext_dir
+        for _, pkg in ipairs(package.load_packages({"gettext"})) do
+            if pkg:requireinfo().originstr == "gettext" then
+                gettext_dir = pkg:installdir() .. "/bin/"
+            end
         end
-        local potloc = i18n_dir .. "pot"
+
+        local i18n_dir = "i18n/"
+        local potloc = i18n_dir .. what_for .. ".pot"
 
         if what_to_do == "pot" then
             if not option.get("no-rebuild") then
@@ -70,41 +70,54 @@ task("i18n")
                 task.run("build", { target = what_for })
             end
 
-            print("creating templates...")
-            local rsjpath = (what_for == "API") and "src/shared" or
-                format(
-                    "%s/.objs/" .. what_for .. "/%s/%s/%s",
-                    config.builddir(), config.plat(),
-                    config.arch(), config.mode()
-                )
-            -- print(rsjpath)
-            i18n.generate_pot(rsjpath, potloc, what_for ~= "API")
+            print("creating " .. potloc .. "...")
+            local rsjpath = format(
+                "%s/.objs/" .. what_for .. "/%s/%s/%s",
+                config.builddir(), config.plat(),
+                config.arch(), config.mode()
+            )
+            print('RSJ path: ' .. rsjpath)
+            if os.isfile(potloc) then
+                 os.rm(potloc)
+            end
+            os.touch(potloc)
+
+            local xgettext = gettext_dir .. "xgettext"
+            for __, fullpath in ipairs(os.files(rsjpath .. "/*.rsj"))
+            do
+                local fn = path.filename(fullpath)
+                if not fn:startswith("small") then
+                    print("found " .. fullpath .. " for localization")
+                    os.execv(xgettext, { "-o", potloc, "-E", "-i",
+                                         "--add-comments=TRANSLATORS",
+                                         "--sort-by-file", "--omit-header",
+                                         "-j", fullpath })
+                end
+            end
 
         elseif what_to_do == "po" then
             for _, fullpath in ipairs(os.dirs(i18n_dir .. "*")) do
-                local out = fullpath .. "/po"
+                local out = fullpath .. "/" .. what_for .. ".po"
                 print("merging " .. potloc .. " to " .. out)
-                i18n.merge_po_files(potloc, out)
+
+                if os.exists(out) then
+                    os.execv(gettext_dir .. "msgmerge", { "-i", "-E", "-N", "-U", out, potloc })
+                else
+                    os.cp(potloc, out)
+                end
             end
 
         elseif what_to_do == "mo" then
             for _, fullpath in ipairs(os.dirs(i18n_dir .. "*")) do
-                local outpath = fullpath .. "/mo"
-                local inpaths = { fullpath .. "/po" }
-
-                if what_for ~= "API" then
-                    local language = path.filename(fullpath)
-                    table.insert(inpaths, "i18n/" .. language .. "/po")
-                end
+                local outpath = fullpath .. "/" .. what_for .. ".mo"
+                local inpath = fullpath .. "/" .. what_for .. ".po"
 
                 print("creating " .. outpath .. " ...")
-                i18n.compile_po_files(inpaths, outpath)
+                os.execv(gettext_dir .. "msgfmt", table.join(inpath, { "-o", outpath }))
             end
 
         elseif what_to_do == "clean" then
-            for _, path in ipairs({ "**/*.mo" }) do
-                os.rm(path)
-            end
+            os.rm(fullpath .. "/" .. what_for .. ".mo")
 
         else
             os.exec("xmake i18n --help")
--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,8 @@ includes("options.lua")
 add_imports("lib.detect.find_program")
 add_moduledirs(os.projectdir() .. "/build-aux")
 add_imports("miscs")
-add_requires("lua", { system = false })
+add_requires("lua 5.5.0", { system = false })
+add_requires("gettext", { system = false })
 add_rules("mode.debug", "mode.release")
 
 local rel_type = 'a' -- or b(eta) or r(c) - empty for stable
@@ -73,7 +74,7 @@ for _, dir in ipairs(os.dirs("src/*")) do
             -- Where to search for compiled i18n - that %s is kept for FCL's gettext
             os.setenv('LOC_PATH',
                 install and "/usr/share/locale/%s/LC_MESSAGES/" .. name .. ".mo"
-                        or os.projectdir() .. "/src/" .. name .. "/i18n/%s/cc.mo")
+                        or os.projectdir() .. "/i18n/%s/" .. name .. ".mo")
 
             -- Remove the dummy file
             local targetfile = target:targetfile()
@@ -82,67 +83,14 @@ for _, dir in ipairs(os.dirs("src/*")) do
             end
         end)
 
-        after_build( function (target)
-            -- PPUDump options (must be put before file names)
-            --     -F<format>  Set output format to <format>: we only care about j(SON)
-            --     -M Exit with ExitCode=2 if more information is available
-            --     -S Skip PPU version check. May lead to reading errors
-            --     -V<verbose>  Set verbosity to <verbose>
-            --                    H - Show header info
-            --                    I - Show interface
-            --                    M - Show implementation
-            --                    S - Show interface symbols
-            --                    D - Show interface definitions
-            --                    A - Show all
-            local ppudump = find_program("ppudump", { check = "-h" })
-            local out, err = os.iorunv(ppudump, table.join(
-                { "-VI", "-Fj" },
-                os.files(target:objectdir() .. "/*.ppu"))
-            )
-            if not miscs.is_string_empty(err) then
-                print("error dumping infos from PPUs:")
-                print(err)
-                return
-            end
-
-            import("core.base.json")
-            local depends = os.files("src/" .. name .. "/*", true)
-
-            -- All programs use cc.getopts and have localizations
-            table.insert(depends, "include/cc.getopts.inc")
-            table.insert(depends, "include/i18n.inc")
-            depends = table.join(depends, os.files("src/shared/argpas/*.pp"))
-
-            -- Read the dumped PPU data. It is an array with the following keys
-            -- Files: array of files that made the unit. Unused as there is no edge-cases
-            -- Units: used units in both interface and implementation sections
-            -- Ignore everything else. RTL, FCL etc units are not included in the depends table.
-            for _, obj in ipairs(json.decode(out)) do
-                for __, unit in ipairs(obj["Units"]) do
-                    if unit:startswith("cc.") or unit:startswith("lua") or
-                       unit == "lauxlib" then
-                        table.insert(depends, "include/" .. unit .. ".inc")
-                        table.insert(depends, "src/shared/" .. unit .. ".pp")
-                    elseif unit == "i18n" then
-                        table.insert(depends, "src/" .. name .. "/i18n.inc")
-                    end
-                end
-            end
-
-            -- target:dependfile() contains serialized Lua table (lol)
-            local depf = io.load(target:dependfile())
-            depf["files"] = table.unique(table.join(depends, depf["files"]))
-            io.writefile(target:dependfile(), string.serialize(depf))
-        end)
-
         before_install( function (target)
             import("core.base.task")
             install = true
             task.run("build", { target = name })
             task.run("i18n", { task = "all", what = name, no_rebuild = true })
             task.run("docs", { task = "build", what = name })
-            for __, fullpath in ipairs(os.dirs("src/" .. name .. "/i18n/*")) do
-                target:add("installfiles", fullpath .. "/cc.mo", {
+            for __, fullpath in ipairs(os.dirs("i18n/*")) do
+                target:add("installfiles", fullpath .. "/" .. name .. ".mo", {
                     prefixdir = "share/locale/" .. path.filename(fullpath) .. "/LC_MESSAGES/",
                     filename = name .. ".mo"
                 })
@@ -153,65 +101,5 @@ for _, dir in ipairs(os.dirs("src/*")) do
 
     ::continue::
 end
-
-target("API")
-    set_kind("phony")
-    set_default(false)
-
-    on_install( function (_)
-        raise(
-            "installation for this target is NOT supported!\n" ..
-            "What to install are src/shared/cc.*.{o,ppu}. Install to a " ..
-            "supported directory in your fpc.cfg."
-        )
-    end)
-
-    on_build( function (_)
-        local args = { "@cc.cfg", miscs.get_custom_fpc_conf() }
-        if is_mode("debug") then
-            table.insert(args, "-dDEBUG")
-        end
-
-        for __, fullpath in ipairs(os.files("src/shared/cc.*.pp")) do
-            import("core.project.config")
-
-            local firstln = table.to_array(io.lines(fullpath))[1]:split(' ')
-            if firstln[2] == "SKIP" then -- Platform-specific uni
-                if table.contains(firstln, config.plat()) then
-                    cprint("${yellow}skipping " .. fullpath ..
-                           " for not supporting " .. config.plat() .. "${clear}")
-                    goto continue
-                end
-            elseif firstln[2] == "ALLOW" then
-                if not table.contains(firstln, config.plat()) then
-                    cprint("${yellow}skipping " .. fullpath ..
-                           " for not supporting " .. config.plat() .. "${clear}")
-                    goto continue
-                end
-            end
-
-            print("compiling " .. fullpath .. " ...")
-            local compiler = config.get("pc")
-            -- tested: errors will still be printed to XMake
-            -- however warnings, infos and such will not be shown
-            os.runv(miscs.is_string_empty(compiler) and "fpc" or compiler,
-                    table.join(args, fullpath))
-
-            ::continue::
-        end
-    end)
-
-    on_clean( function (_)
-        os.rm("i18n/*/cc.mo")
-        os.rm("src/shared/*.o")
-        os.rm("src/shared/*.ppu")
-        os.rm("src/shared/*.rsj")
-        os.rm("src/shared/libimp*.a")
-    end)
-
-target("programs")
-	set_kind("phony")
-	set_default(true)
-    add_deps(programs)
 
 includes("i18n/xmake.lua", "docs/xmake.lua")
